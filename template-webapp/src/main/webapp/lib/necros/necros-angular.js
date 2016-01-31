363ef130f9483abe0f11286eaf874b94652b6cb5
weiht <weihthchk@hotmail.com>
2016-01-31 22:27:10 +0800

    Migrated nSection directive from hats.

--- template-webapp/src/main/webapp/lib/necros/necros-angular.js
+++ template-webapp/src/main/webapp/lib/necros/necros-angular.js
@@ -54,4 +54,96 @@ function isInLayout(el) {
 	return el.parents('.ui-layout-container').size();
 }
 
+//Section (can be used as part of a page, or a modal dialog body)
+
+var sections = [];
+var sectionMap = {};
+var sectionDef = [function() {
+	return sectionMap;
+}];
+
+function addSection(sec) {
+	//TODO prevent duplications
+	sections.push(sec);
+	sectionMap[sec.name] = sec;
+}
+
+var nSectionsDef = [function() {
+	this.add = addSection;
+	this.$get = sectionDef;
+}];
+
+mod.provider('nSections', nSectionsDef);
+mod.provider('sections', nSectionsDef);
+mod.service('sectionConfigService', function() {
+	return {
+		addSection: addSection
+	}
+});
+
+var nSectionDef = ['$injector', '$interpolate', '$http', '$compile', '$controller', '$templateCache', '$timeout',
+                 function($injector, $interpolate, $http, $compile, $controller, $templateCache, $timeout) {
+	return {
+		restrict: 'A',
+	    terminal: true,
+	    priority: -400,
+	    scope: true,
+		compile: function(cElement, cAttrs) {
+			function showTemplate(template, scope, element, attrs, sec) {
+				element.html(template);
+				if (sec.controller) {
+					var $scope = scope.$new();
+					$controller(sec.controller, {$scope: $scope});
+					$compile(element.contents())($scope);
+					//TODO Find a chance to destroy this $scope.
+				} else {
+					$compile(element.contents())(scope);
+				}
+			}
+			
+			function render(scope, element, attrs, sec) {
+				if (sec.template) {
+					showTemplate(sec.template, scope, element, attrs, sec);
+				} else if (sec.templateUrl) {
+					$http.get(sec.templateUrl, {cache: $templateCache})
+						.success(function(template) {
+							sec.template = template;
+							showTemplate(template, scope, element, attrs, sec);
+						});
+				}
+			}
+			
+			return function(scope, element, attrs) {
+				attrs.$observe('nSection', function() {
+					var sec = sectionMap[attrs.nSection];
+					if (!sec) {
+						try {
+							sec = scope.$eval(sec);
+						} catch (e) {}
+					}
+					if (!sec) return;
+					if (!sec.__rendered && sec.requireModule && typeof window.require == 'function') {
+						require(sec.requireModule.split(/[\,,\;,\s]/), function($scope, $element, $attrs, $sec) {
+							return function() {
+								$timeout(function() {
+									render($scope, $element, $attrs, $sec);
+									$sec.__rendered = true;
+								});
+							};
+						}(scope, element, attrs, sec));
+					} else {
+						render(scope, element, attrs, sec);
+						sec.__rendered = true;
+					}
+				});
+			}
+		}
+	};
+}];
+
+mod.directive('nSection', nSectionDef);
+mod.directive('section', nSectionDef);
+
+// End of section
+
 });
